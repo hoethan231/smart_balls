31dc3868f94099de9df3838a58136a8c7112aa32
hoethan231 <ethanho231@gmail.com>
2025-10-15 17:47:32 -0700

    beep buzzer

--- Advanced_I2C/Advanced_I2C.ino
+++ Advanced_I2C/Advanced_I2C.ino
@@ -1,13 +1,13 @@
 #include "ICM20689.h"
-
 // an ICM20689 object with the ICM20689 sensor on I2C bus 0 with address 0x68
-ICM20689 IMU(Wire,0x68);
+ICM20689 IMU(Wire, 0x68);
 int status;
+
 
 void setup() {
   // serial to display data
   Serial.begin(115200);
-  while(!Serial) {}
+  while (!Serial) {}
 
   // start communication with IMU
   status = IMU.begin();
@@ -16,7 +16,7 @@ void setup() {
     Serial.println("Check IMU wiring or try cycling power");
     Serial.print("Status: ");
     Serial.println(status);
-    while(1) {}
+    while (1) {}
   }
   // setting the accelerometer full scale range to +/-8G
   IMU.setAccelRange(ICM20689::ACCEL_RANGE_8G);
@@ -27,25 +27,36 @@ void setup() {
   // setting SRD to 19 for a 50 Hz update rate
   IMU.setSrd(19);
   Serial.println("ax,ay,az,gx,gy,gz,temp_C");
+
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // read the sensor
   IMU.readSensor();
 
+  float ay = IMU.getAccelY_mss();
+  float absAy = fabsf(ay);
+
+  if (absAy > 5.0f) {
+    digitalWrite(2, HIGH);
+  } else {
+    digitalWrite(2, LOW);
+  }
+
   // display the data
-  Serial.print(IMU.getAccelX_mss(),6);
+  Serial.print(IMU.getAccelX_mss(), 6);
   Serial.print("\t");
-  Serial.print(IMU.getAccelY_mss(),6);
+  Serial.print(IMU.getAccelY_mss(), 6);
   Serial.print("\t");
-  Serial.print(IMU.getAccelZ_mss(),6);
+  Serial.print(IMU.getAccelZ_mss(), 6);
   Serial.print("\t");
-  Serial.print(IMU.getGyroX_rads(),6);
+  Serial.print(IMU.getGyroX_rads(), 6);
   Serial.print("\t");
-  Serial.print(IMU.getGyroY_rads(),6);
+  Serial.print(IMU.getGyroY_rads(), 6);
   Serial.print("\t");
-  Serial.print(IMU.getGyroZ_rads(),6);
+  Serial.print(IMU.getGyroZ_rads(), 6);
   Serial.print("\t");
-  Serial.println(IMU.getTemperature_C(),6);
+  Serial.println(IMU.getTemperature_C(), 6);
   delay(20);
 }
